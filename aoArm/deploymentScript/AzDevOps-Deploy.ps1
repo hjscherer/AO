497e6084ad05e846ca8d92291530a3d5dedc0af6
hscherer <hscherer@microsoft.com>
2021-11-16 21:03:27 +0100

    variables

--- aoArm/deploymentScript/AzDevOps-Deploy.ps1
+++ aoArm/deploymentScript/AzDevOps-Deploy.ps1
@@ -31,9 +31,9 @@ param
   # Whether pipelines should skip running immediately after import. Pass $true to skip (NOT run) pipelines immediately.
   [bool] $SkipFirstPipelineRun = $true,
   # Username provisioned in AAD B2C to use for smoke tests
-  [string] $SmokeTestUserName
+  [string] $SmokeTestUserName,
   # Hashtable of key-value pairs to add to the created Variable Group as individual Variables
-  #$AzDevOpsVariables
+  $AzDevOpsVariables
 )
 
 # ##################################################
