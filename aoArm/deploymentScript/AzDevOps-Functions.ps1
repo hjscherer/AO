eccd44973e503e7366db124a9f8b48eff864e6b1
hscherer <hscherer@microsoft.com>
2021-11-16 13:53:58 +0100

    update

--- aoArm/deploymentScript/AzDevOps-Functions.ps1
+++ aoArm/deploymentScript/AzDevOps-Functions.ps1
@@ -58,9 +58,11 @@ function New-AzDevOpsProject {
 
   # Check for existence
   az devops login --organization $azdoOrgUrl
+
   $project = (az devops project show --org $azdoOrgUrl -p $azdoProjectName)
 
   Write-Host $project
+  Write-Host $azdoOrgUrl
 
   (Get-ChildItem -Path Env:AZURE_DEVOPS_EXT_PAT)[0].Value | Write-Host
 
