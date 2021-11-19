b2bc366bfcb37adcfe2ede1e0fd78f5b6fd11502
hscherer <hscherer@microsoft.com>
2021-11-19 11:42:30 +0100

    update

--- aoArm/deploymentScript/AzDevOps-Deploy.ps1
+++ aoArm/deploymentScript/AzDevOps-Deploy.ps1
@@ -44,7 +44,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 # Install-Module -Name PowerShellForGitHub,PSSodium -Confirm:$false -Force
 # Import-Module -Name PowerShellForGitHub,PSSodium
 
-dnf install git-all
+apt-get install git
 
 # Set Azure CLI to auto install extensions
 az config set extension.use_dynamic_install=yes_without_prompt
